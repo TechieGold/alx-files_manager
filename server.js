c16f7d571bd95fcc87da7c90e512dfaa80dbef98
Gold Israel <techiegold101@gmail.com>
2024-05-14 23:09:53 +0100

    Create and configure the Express server

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+const express = require('express');
+const routes = require('./routes/index');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
